e1b2955f393885dd3a514c4119443802636cf866
Ulzhan Koken <kokenova.ulzhan@gmail.com>
2025-03-09 01:36:46 +0500

    add first task

---
+++ practisingReact/useState/practise/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TextInput from './components/Task1TextInput'
+
+function App() {
+
+
+  return (
+    <>
+      <TextInput/>
+    </>
+  )
+}
+
+export default App
